ad3f7e540f60b36791308496f2c8f9557e60c965
sudharaD <72960779+sudharaD@users.noreply.github.com>
2023-12-26 15:33:33 +0530

    Did adjustments
    remove recursion function, fixed overide image names, now program stops after download comple

--- image_downloader.py
+++ image_downloader.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
-class ImageDownloader():
-    def __init__(self, url, output_folder, keywords, max_depth=2, max_images=150):
+
+class ImageDownloader:
+    def __init__(
+        self, url, output_folder, keywords, max_images=150
+    ):  # Add max depth if want to go deep | ex: max_depth=2
         self.url = url
         self.output_folder = output_folder
         self.keywords = keywords
-        self.max_depth = max_depth
+
+        #  Add max depth
+        # self.max_depth = max_depth
+
         self.max_images = max_images
         self.download_count = 0  # Initialize a global count variable
 
@@ -19,6 +25,7 @@ class ImageDownloader():
         This method initiates the download process by creating the output folder,
         setting up a visited_urls set to avoid duplicate downloads, and calling
         the internal recursive function for processing the specified URL.
+        (If want to call the recursive function remove the related comments)
 
         Returns:
             None
@@ -26,45 +33,72 @@ class ImageDownloader():
         os.makedirs(self.output_folder, exist_ok=True)
         visited_urls = set()
 
-        def download_images_internal(current_url, depth):
+        # if want to go deep initiate attribute to catch deapth | ex: def download_images_internal(current_url, depth):
+        def download_images_internal(current_url):
             """
             Recursively processes a web page, downloads images, and follows links.
 
             Args:
                 current_url (str): The URL of the current web page.
                 depth (int): The depth of the recursion, representing the level of links.
+                (Reactive recursion if want to go deep)
 
             Returns:
                 None
             """
-            if depth > self.max_depth or current_url in visited_urls or self.download_count >= self.max_images:
-                return
+            # Codes related to go deep
+            # if (
+            #     depth > self.max_depth
+            #     or current_url in visited_urls
+            #     or self.download_count >= self.max_images
+            # ):
+            #     return
 
-            visited_urls.add(current_url)
+            # debugging statement - Print the current URL for debugging
+            print(f"Processing: {current_url}")
+
+            # Codes related to go deep
+            # visited_urls.add(current_url)
+
             response = requests.get(current_url)
-            soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, "html.parser")
 
             # Find images with alt attribute containing any of the keywords
-            image_tags = soup.find_all('img', {'alt': lambda x: x and any(keyword.lower() in x.lower() for keyword in self.keywords)})
+            image_tags = soup.find_all(
+                "img",
+                {
+                    "alt": lambda x: x
+                    and any(keyword.lower() in x.lower() for keyword in self.keywords)
+                },
+            )
             for img_tag in image_tags:
-                img_url = img_tag.get('src')
-                if img_url and self.download_count < self.max_images:
+                img_url = img_tag.get("src")
+                # if img_url and self.download_count < self.max_images:
+                if (
+                    img_url
+                    and self.download_count < self.max_images
+                    and not img_url.startswith("data:image")
+                ):
                     img_url = urljoin(current_url, img_url)
                     img_data = requests.get(img_url).content
                     # Construct a unique filename based on search keywords and index
-                    img_filename = os.path.join(self.output_folder, f'{"_".join(self.keywords)}_{self.download_count + 1}.jpg')
-                    with open(img_filename, 'wb') as img_file:
+                    domain_name = urlparse(current_url).netloc.replace(".", "_")
+                    img_filename = os.path.join(
+                        self.output_folder,
+                        # f'{"_".join(self.keywords)}_{self.download_count + 1}.jpg',
+                        f'{domain_name}_{"_".join(self.keywords)}_{self.download_count + 1}.jpg',
+                    )
+                    with open(img_filename, "wb") as img_file:
                         img_file.write(img_data)
-                    print(f'Downloaded: {img_filename}')
+                    print(f"Downloaded: {img_filename}")
                     self.download_count += 1
 
+            # If want to go deep activate the recursive function
             # Follow links recursively
-            for link in soup.find_all('a', href=True):
-                next_url = urljoin(current_url, link['href'])
-                download_images_internal(next_url, depth + 1)
+            # for link in soup.find_all("a", href=True):
+            #     next_url = urljoin(current_url, link["href"])
+            #     download_images_internal(next_url, depth + 1)
 
-        download_images_internal(self.url, 0)
-
-
-
-
+        download_images_internal(
+            self.url
+        )  # send the depth value if want to go deep: ex:(self.url, 0)
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from image_downloader import ImageDownloader
 
-if __name__ == '__main__':
-    url_to_scrape = 'https://en.wikipedia.org/wiki/Elon_Musk'
-    output_directory = 'data/elon_musk'
-    search_keywords = ['elon musk', 'elon', 'musk']
-    max_depth = 2
+if __name__ == "__main__":
+    url_to_scrape = "https://en.wikipedia.org/wiki/Elon_Musk"
+    output_directory = "data/elon_musk"
+    search_keywords = ["elon musk", "elon", "musk"]
+
+    # if want to go deep add depth
+    # max_depth = 2
     max_images_count = 150
 
-    image_download = ImageDownloader(url_to_scrape, output_directory, search_keywords, max_depth, max_images=max_images_count)
+    image_download = ImageDownloader(
+        url_to_scrape,
+        output_directory,
+        search_keywords,
+        # max_depth,
+        max_images=max_images_count,
+    )
     image_download.download_images_with_keywords()
